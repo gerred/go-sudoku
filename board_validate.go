bc09041275e42910d51ecce6e817ae98e27340e6
Jud White <github@judsonwhite.com>
2015-11-08 18:53:20 -0600

    add check for missing hints in per-move validator

--- board_validate.go
+++ board_validate.go
@@ -4,25 +4,37 @@ import "fmt"
 
 func (b *board) Validate() error {
 	for pos := 0; pos < 81; pos++ {
+		var blit uint
+
 		// validate row
 		rowVals := make([]uint, 9)
 		startRow := (pos / 9) * 9
+		blit = 0
 		for r := startRow; r < startRow+9; r++ {
 			rowVals[r-startRow] = b.solved[r]
+			blit |= b.blits[r]
 		}
 		if err := validate(rowVals); err != nil {
 			return err
+		}
+		if !b.loading && blit != 0x1FF {
+			return fmt.Errorf("row missing hint %#2v %09b", getCoords(pos), blit)
 		}
 
 		// validate column
 		colVals := make([]uint, 9)
 		colIndex := 0
+		blit = 0
 		for c := pos % 9; c < 81; c += 9 {
 			colVals[colIndex] = b.solved[c]
 			colIndex++
+			blit |= b.blits[c]
 		}
 		if err := validate(colVals); err != nil {
 			return err
+		}
+		if !b.loading && blit != 0x1FF {
+			return fmt.Errorf("col missing hint %#2v %09b", getCoords(pos), blit)
 		}
 
 		// validate box
@@ -30,14 +42,19 @@ func (b *board) Validate() error {
 		startCol := ((pos % 9) / 3) * 3
 		boxVals := make([]uint, 9)
 		boxIndex := 0
+		blit = 0
 		for r := startRow; r < startRow+3; r++ {
 			for c := startCol; c < startCol+3; c++ {
 				boxVals[boxIndex] = b.solved[r*9+c]
 				boxIndex++
+				blit |= b.blits[r*9+c]
 			}
 		}
 		if err := validate(boxVals); err != nil {
 			return err
+		}
+		if !b.loading && blit != 0x1FF {
+			return fmt.Errorf("box missing hint %#2v %09b", getCoords(pos), blit)
 		}
 	}
 
