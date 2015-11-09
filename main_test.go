9f143912d5fb10469f0922c2c1b78f3709fad402
Jud White <github@judsonwhite.com>
2015-11-09 04:27:02 -0600

    add test boards

--- main_test.go
+++ main_test.go
@@ -1,10 +1,12 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestBoards(t *testing.T) {
 	files := []string{
-		"./test_files/input_17_clues.txt",
 		"./test_files/01_naked_single_493382.txt",
 		"./test_files/02_hidden_single_1053217.txt",
 		"./test_files/03_naked_pair_1053222.txt",
@@ -12,14 +14,21 @@ func TestBoards(t *testing.T) {
 		"./test_files/05_naked_quint_1051073.txt",
 		"./test_files/06_hidden_pair_1208057.txt",
 		"./test_files/07_hidden_triple_188899.txt",
-		"./test_files/input_supposedly_hard.txt",
 		"./test_files/08_hidden_quint_188899.txt",
 		"./test_files/09_pointing_pair_and_triple_1011509.txt",
 		"./test_files/10_xwing_1307267.txt",
-		"./test_files/12_tough_20151107_173.txt", // TODO
+		//"./test_files/12_tough_20151107_173.txt", // TODO
 		"./test_files/11_swordfish_1280430.txt",
 		"./test_files/13_swordfish_008009000300057001000100009230000070005406100060000038900003000700840003000700600.txt",
 		"./test_files/14_swordfish_980010020002700000000009010700040800600107002009030005040900000000005700070020039.txt",
+		"./test_files/15_swordfish_108000067000050000000000030006100040450000900000093000200040010003002700807001005.txt",
+		"./test_files/16_swordfish_107300040800006000050870630090000510000000007700060080000904000080100002410000000.txt",
+		"./test_files/17_swordfish_300040000000007048000000907010003080400050020050008070500300000000000090609025300.txt",
+		"./test_files/18_swordfish.txt",
+		"./test_files/19_supposedly_hard.txt",
+		"./test_files/20_17_clues.txt",
+		"./test_files/21_ywing.txt",
+		"./test_files/22_xychain.txt",
 	}
 
 	for _, file := range files {
@@ -39,4 +48,5 @@ func TestBoards(t *testing.T) {
 			return
 		}
 	}
+	fmt.Printf("solved %d puzzles\n", len(files))
 }
