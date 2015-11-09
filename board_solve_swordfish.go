d638f093a5bf3fd9bb29101ec697e9640b76b094
Jud White <github@judsonwhite.com>
2015-11-08 18:52:28 -0600

    sword fish

--- board_solve_swordfish.go
+++ board_solve_swordfish.go
@@ -2,76 +2,411 @@ package main
 
 import (
 	"fmt"
+	"reflect"
+	"sync"
 
 	"github.com/judwhite/go-sudoku/internal/bits"
 )
+
+type swordfishOperation struct {
+	blockType                    string
+	op                           containerOperator
+	op_inverted                  containerOperator
+	nextContainer                func(int) int
+	doesOverlap                  func(x int, y int) bool
+	isInSameDimension            func(x int, y int) bool
+	maxMisses                    func(overlapSet []int) int
+	getMaxPosFromOverlapSet      func(overlapSet []int) int
+	getInvertedDimensionPosition func(pos int) int
+}
 
 func (b *board) SolveSwordFish() error {
 	// http://www.sudokuwiki.org/Sword_Fish_Strategy
 	// find a 3x3 which share the same candidate
 	// hint cannot be repeated on container (row or col depending on orientation)
 	// 333,332,322,222 = all valid.
-	for i := 0; i < 81; i++ {
-		if b.solved[i] != 0 {
+
+	// create dimensions for looking for SwordFish in the row and column dimension
+	dims := []swordfishOperation{
+		{
+			blockType:   "row",
+			op:          b.operateOnRow,
+			op_inverted: b.operateOnColumn,
+			nextContainer: func(cur int) int {
+				// next start row index
+				next := cur + 9
+				if next >= 81 {
+					return -1
+				}
+				return next
+			},
+			doesOverlap: func(x int, y int) bool {
+				coords1 := getCoords(x)
+				coords2 := getCoords(y)
+				return coords1.col == coords2.col
+			},
+			isInSameDimension: func(x int, y int) bool {
+				coords1 := getCoords(x)
+				coords2 := getCoords(y)
+				return coords1.row == coords2.row
+			},
+			maxMisses: func(overlapSet []int) int {
+				// count distinct cols
+				cols := make(map[int]interface{})
+				for _, pos := range overlapSet {
+					cols[getCoords(pos).col] = struct{}{}
+				}
+				return 3 - len(cols)
+			},
+			getMaxPosFromOverlapSet: func(overlapSet []int) int {
+				maxRow := 0
+				for _, pos := range overlapSet {
+					coords := getCoords(pos)
+					if coords.row > maxRow {
+						maxRow = coords.row
+					}
+				}
+				return maxRow * 9
+			},
+			getInvertedDimensionPosition: func(pos int) int {
+				return getCoords(pos).col
+			},
+		},
+		{
+			blockType:   "column",
+			op:          b.operateOnColumn,
+			op_inverted: b.operateOnRow,
+			nextContainer: func(cur int) int {
+				// next start column index
+				next := cur + 1
+				if cur >= 9 {
+					return -1
+				}
+				return next
+			},
+			doesOverlap: func(x int, y int) bool {
+				coords1 := getCoords(x)
+				coords2 := getCoords(y)
+				return coords1.row == coords2.row
+			},
+			isInSameDimension: func(x int, y int) bool {
+				coords1 := getCoords(x)
+				coords2 := getCoords(y)
+				return coords1.col == coords2.col
+			},
+			maxMisses: func(overlapSet []int) int {
+				// count distinct rows
+				rows := make(map[int]interface{})
+				for _, pos := range overlapSet {
+					rows[getCoords(pos).row] = struct{}{}
+				}
+				return 3 - len(rows)
+			},
+			getMaxPosFromOverlapSet: func(overlapSet []int) int {
+				maxCol := 0
+				for _, pos := range overlapSet {
+					coords := getCoords(pos)
+					if coords.col > maxCol {
+						maxCol = coords.col
+					}
+				}
+				return maxCol
+			},
+			getInvertedDimensionPosition: func(pos int) int {
+				return getCoords(pos).row
+			},
+		},
+	}
+
+	for _, dim := range dims {
+
+		// TODO: DEBUG, remove
+		/*if dim.blockType != "column" {
 			continue
-		}
+		}*/
+		// END debug
 
-		//coords := getCoords(i)
+		for i := 0; i != -1; i = dim.nextContainer(i) {
+			// i = start of row/column
 
-		// create dimensions for looking for SwordFish in the row and column dimension
-		dims := []struct {
-			op          func(int, func(int, int) error) error
-			op_inverted func(int, func(int, int) error) error
-		}{
-			{
-				op:          b.operateOnRow,
-				op_inverted: b.operateOnColumn,
-			},
-			{
-				op:          b.operateOnColumn,
-				op_inverted: b.operateOnRow,
-			},
-		}
+			// TODO: debug, remove
+			/*if i != 2 {
+				continue
+			}*/
+			// END debug
 
-		for dim_it, dim := range dims {
-			bitList := bits.GetBitList(b.blits[i])
+			// hint, list of positions
+			initialCandidates := make(map[uint][]int)
 
-			var pickList []int
-
-			for _, bit := range bitList {
-				getPickList := func(target int, source int) error {
-					if b.solved[target] != 0 {
-						return nil
-					}
-					if b.blits[target]&bit == bit {
-						pickList = append(pickList, target)
-					}
+			// operate on all cells in container
+			// get list of candidate cells: map(hint, []pos)
+			extractHints := func(target int, source int) error {
+				bitList := bits.GetBitList(b.blits[target])
+				if len(bitList) < 2 {
 					return nil
 				}
 
-				if err := dim.op(i, getPickList); err != nil {
-					return err
+				for _, bit := range bitList {
+					list, ok := initialCandidates[bit]
+					if !ok {
+						initialCandidates[bit] = []int{target}
+					} else {
+						initialCandidates[bit] = append(list, target)
+					}
 				}
+				return nil
+			}
 
-				if len(pickList) != 2 && len(pickList) != 3 {
+			if err := dim.op(i, extractHints); err != nil {
+				return err
+			}
+
+			// get a list of hints containing permutations of 2 or 3 cells
+			candidatePerms := swordfishGetCandidatePermutations(initialCandidates)
+			if len(candidatePerms) == 0 {
+				continue
+			}
+
+			fmt.Printf("%#v\n", candidatePerms)
+
+			for hint, v := range candidatePerms {
+				// TODO: REMOVE, debug
+				/*if hint != 1 {
 					continue
+				}*/
+				// END debug
+
+				var once1 sync.Once
+				print1 := func() {
+					fmt.Printf("-----------------------------------------\n")
+					fmt.Printf("swordfish: dim:%s num:%d\n", dim.blockType, bits.GetSingleBitValue(hint))
 				}
 
-				// pickList contains all cells in the row/col container with
-				// the current `bit` in its hints. this number has to be 2 or 3.
-				fmt.Printf("swordfish: dim:%d num:%d\n", dim_it, bits.GetSingleBitValue(bit))
-				for _, pick := range pickList {
-					fmt.Printf("- %#2v %s\n", getCoords(pick), bits.GetString(b.blits[pick]))
-				}
+				for _, c := range v {
+					// TODO: DEBUG, remove
+					/*if !reflect.DeepEqual(c, []int{2, 11}) {
+						continue
+					}
+					fmt.Println("GOT IT")*/
+					// END debug
 
-				// get other rows/cols
+					var once2 sync.Once
+					print2 := func() {
+						once1.Do(print1)
+						fmt.Printf("\n")
+						fmt.Printf("-- top level:\n")
+					}
+
+					sfPerms, err := b.swordfishGetPermutations(dim, c, hint, i)
+					if err != nil {
+						return err
+					}
+
+					var once3 sync.Once
+					print3 := func() {
+						once2.Do(print2)
+						for _, pick := range c {
+							fmt.Printf("-- %#2v %s\n", getCoords(pick), bits.GetString(b.blits[pick]))
+						}
+					}
+
+					for _, x := range sfPerms {
+						// TODO: DEBUG, remove
+						/*if !reflect.DeepEqual(x, []int{3, 12, 30}) {
+							continue
+						}*/
+						// END debug
+
+						var once4 sync.Once
+						print4 := func() {
+							once3.Do(print3)
+							fmt.Printf("---- second level:\n")
+							for _, pos := range x {
+								fmt.Printf("---- %#2v %s\n", getCoords(pos), bits.GetString(b.blits[pos]))
+							}
+						}
+
+						// combine first and second level to get new 'one must overlap' set
+						// (in this case, two must overlap...)
+						var overlapSet []int
+						overlapSet = append(overlapSet, c...)
+						overlapSet = append(overlapSet, x...)
+
+						fmt.Printf("%#v\n", overlapSet)
+
+						nextPos := dim.getMaxPosFromOverlapSet(overlapSet)
+						sfPerms2, err := b.swordfishGetPermutations(dim, overlapSet, hint, nextPos)
+						if err != nil {
+							return err
+						}
+
+						if len(sfPerms2) != 0 {
+							once4.Do(print4)
+							for _, y := range sfPerms2 {
+								// TODO: DEBUG, remove
+								/*if !reflect.DeepEqual(y, []int{16, 34}) {
+									continue
+								}*/
+								// END debug
+
+								fmt.Printf("------ third level:\n")
+								for _, pos := range y {
+									fmt.Printf("------ %#2v %s\n", getCoords(pos), bits.GetString(b.blits[pos]))
+								}
+
+								// Here we go...
+								// if we pulled sets from columns, given a hint, remove that hint
+								// from all cells in the superset of ROWS covered by our swordfish
+								// set. still confused? me too.
+								// http://www.sudokuwiki.org/Sword_Fish_Strategy
+
+								// brain needs a break, brb.
+								//dim.op_inverted
+								modified, err := b.swordfishApply(dim, hint, c, x, y)
+								if err != nil {
+									return err
+								}
+								if modified {
+									// try simpler techniques before re-trying swordfish
+									fmt.Println("**** we did it!")
+									return nil
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
+
 	return nil
 }
 
-func (b *board) getSwordFishContainer(bit uint, rowcol int, inspect inspector) error {
-	// TODO
-	return nil
+func (b *board) swordfishGetPermutations(dim swordfishOperation, mustOverlap []int, hint uint, pos int) ([][]int, error) {
+	var perms [][]int
+
+	// make sure we're still on the board
+	nextPos := dim.nextContainer(pos)
+	if nextPos == -1 {
+		return perms, nil
+	}
+
+	var candidates []int
+	getCandidates := func(target int, source int) error {
+		// cell must contain the hint we're looking for
+		if b.blits[target]&hint != hint {
+			return nil
+		}
+
+		candidates = append(candidates, target)
+		return nil
+	}
+
+	if err := dim.op(nextPos, getCandidates); err != nil {
+		return perms, err
+	}
+
+	tmpPerms := swordfishGetTwosAndThrees(candidates)
+	maxMisses := dim.maxMisses(mustOverlap)
+	if maxMisses < 0 {
+		return perms, nil
+	}
+
+	for _, perm := range tmpPerms {
+		misses := 0
+		for _, item := range perm {
+			doesOverlap := false
+			for _, existingSetItem := range mustOverlap {
+				if dim.doesOverlap(item, existingSetItem) {
+					doesOverlap = true
+					break
+				}
+			}
+			if !doesOverlap {
+				misses++
+				if misses > maxMisses {
+					break
+				}
+			}
+		}
+		if misses <= maxMisses {
+			perms = append(perms, perm)
+		}
+	}
+
+	permsN, err := b.swordfishGetPermutations(dim, mustOverlap, hint, nextPos)
+	if err != nil {
+		return perms, err
+	}
+
+	perms = append(perms, permsN...)
+
+	return perms, nil
+}
+
+func swordfishGetCandidatePermutations(orig map[uint][]int) map[uint][][]int {
+	filtered := make(map[uint][][]int)
+	for k, v := range orig {
+		list := swordfishGetTwosAndThrees(v)
+		if len(list) != 0 {
+			filtered[k] = list
+		}
+	}
+	return filtered
+}
+
+func swordfishGetTwosAndThrees(v []int) [][]int {
+	var list [][]int
+
+	fours := getPermutations(4, v, []int{})
+	if len(fours) != 0 {
+		return list
+	}
+
+	threes := getPermutations(3, v, []int{})
+	twos := getPermutations(2, v, []int{})
+
+	list = append(list, threes...)
+	list = append(list, twos...)
+
+	return list
+}
+
+func (b *board) swordfishApply(sf swordfishOperation, hint uint, set1 []int, set2 []int, set3 []int) (bool, error) {
+	var overlap []int
+	overlap = append(overlap, set1...)
+	overlap = append(overlap, set2...)
+	overlap = append(overlap, set3...)
+
+	fmt.Printf("applying, hint: %d...\n", bits.GetSingleBitValue(hint))
+
+	begin := b.blits
+
+	dupeCheck := make(map[int]interface{})
+	for _, item := range overlap {
+		key := sf.getInvertedDimensionPosition(item)
+		_, found := dupeCheck[key]
+		if found {
+			continue
+		}
+		dupeCheck[key] = struct{}{}
+
+		removeHint := func(target int, source int) error {
+			for _, pos := range overlap {
+				if sf.isInSameDimension(target, pos) {
+					return nil
+				}
+			}
+			fmt.Printf("- %#2v %s\n", getCoords(target), bits.GetString(b.blits[target]))
+
+			return b.updateCandidates(target, source, ^hint)
+		}
+
+		fmt.Printf("%#2v %s\n", getCoords(item), bits.GetString(b.blits[item]))
+		if err := sf.op_inverted(item, removeHint); err != nil {
+			return false, err
+		}
+	}
+
+	return !reflect.DeepEqual(begin, b.blits), nil
 }
