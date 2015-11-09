faaf245c7b37264688639a37ceca5ce342051393
Jud White <github@judsonwhite.com>
2015-11-09 04:30:44 -0600

    run many boards from 1 file

--- main.go
+++ main.go
@@ -1,26 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+)
 
 func main() {
-
 	//board, err := getBoard("./test_files/12_tough_20151107_173.txt") // TODO
+	//board, err := getBoard("./test_files/input.txt")
 
-	//board, err getBoard("./test_files/input.txt")
-	board, err := getBoard("./test_files/11_swordfish_1280430.txt")
+	runFile("./test_files/21_ywing.txt")
+
+	//runTop95()
+}
+
+func runFile(fileName string) {
+	board, err := getBoard(fileName)
 	if err != nil {
 		fmt.Printf("ERROR - %s\n", err)
 		return
 	}
 	if err = board.Solve(); err != nil {
 		fmt.Printf("ERROR - %s\n", err)
-		board.Print()
 		board.PrintHints()
 		return
 	}
 
 	if !board.isSolved() {
 		board.PrintHints()
+		board.PrintURL()
+		fmt.Println("could not solve")
+	} else {
+		board.Print()
 	}
-	board.Print()
+}
+
+func runTop95() {
+	b, err := ioutil.ReadFile("./test_files/top95.txt")
+	if err != nil {
+		fmt.Printf("ERROR - %s\n", err)
+		return
+	}
+
+	r := bufio.NewReader(bytes.NewReader(b))
+	line, _ := r.ReadString('\n')
+	for i := 0; line != ""; i++ {
+		// TOOD: debug
+		if i != 6 {
+			line, _ = r.ReadString('\n')
+			continue
+		}
+
+		board, err := loadBoard([]byte(line))
+		if err != nil {
+			board.PrintHints()
+			fmt.Printf("ERROR - %d - %s\n", i+1, err)
+			return
+		}
+		if err = board.Solve(); err != nil {
+			board.PrintHints()
+			fmt.Printf("ERROR - %d - %s\n", i+1, err)
+			return
+		}
+
+		if !board.isSolved() {
+			board.PrintHints()
+			board.PrintURL()
+			fmt.Printf("could not solve - %d\n", i+1)
+			break
+		} else {
+			board.Print()
+		}
+
+		line, _ = r.ReadString('\n')
+	}
 }
